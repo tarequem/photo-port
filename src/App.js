dd97fb8190db65a0e6d4e9522959b3cbda6c71ff
tarequem <moatartareque@gmail.com>
2022-11-16 11:33:13 -0500

    added contact form

--- src/App.js
+++ src/App.js
@@ -13,9 +13,10 @@ function App() {
     { name: 'portraits', description: 'Portraits of people in my life' },
     { name: 'food', description: 'Delicious delicacies' },
     { name: 'landscape', description: 'Fields, farmhouses, waterfalls, and the beauty of nature' },
-  ]); 
+  ]);
 
   const [currentCategory, setCurrentCategory] = useState(categories[0]);
+
   const [contactSelected, setContactSelected] = useState(false);
 
   return (
@@ -24,11 +25,18 @@ function App() {
         categories={categories}
         setCurrentCategory={setCurrentCategory}
         currentCategory={currentCategory}
+        contactSelected={contactSelected}
+        setContactSelected={setContactSelected}
       ></Nav>
       <main>
-        <ContactForm></ContactForm>
-        <Gallery currentCategory={currentCategory}></Gallery>
-        <About></About>
+        {!contactSelected ? (
+          <>
+            <Gallery currentCategory={currentCategory}></Gallery>
+            <About></About>
+          </>
+        ) : (
+          <ContactForm></ContactForm>
+        )}
       </main>
     </div>
   );
